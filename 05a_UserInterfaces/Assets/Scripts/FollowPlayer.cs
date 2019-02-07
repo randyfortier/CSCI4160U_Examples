c4fc25a9f9a7bd9c400f47dfbe0a99867b0ca6de
randyfortier <randy.fortier@gmail.com>
2019-02-07 14:08:11 -0500

    Added starter project for UI demo

---
+++ 05a_UserInterfaces/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+    public Transform target;
+
+    void Update() {
+        Vector3 newCameraPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = newCameraPosition;
+    }
+}
