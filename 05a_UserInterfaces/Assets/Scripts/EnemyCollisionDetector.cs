8a574c9d82afdf91676b406e3be381aabc07e25b
randyfortier <randy.fortier@gmail.com>
2019-02-07 16:24:47 -0500

    Added completed UI demo

---
+++ 05a_UserInterfaces/Assets/Scripts/EnemyCollisionDetector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyCollisionDetector : MonoBehaviour {
+    public Slider playerHealthSlider;
+
+    private CharacterController2D controller;
+
+    private float timeSinceLastTurn;
+    private float turnDuration = 2f;
+    private float velocityX = 2f;
+
+    private void Start() {
+        timeSinceLastTurn = Time.time;
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    private void Update() {
+        if ((Time.time - timeSinceLastTurn) > turnDuration) {
+            // turn
+            velocityX *= -1f;
+
+            // reset timer
+            timeSinceLastTurn = Time.time;
+        }
+
+        controller.Move(velocityX * Time.deltaTime, false, false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            playerHealthSlider.value -= 10;
+            Debug.Log("Ouch!");
+        }
+
+    }
+}
