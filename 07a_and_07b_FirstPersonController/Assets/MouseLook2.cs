bf7bfb6096a53207f43aca2119b254113a747272
randyfortier <randy.fortier@gmail.com>
2019-03-01 17:56:17 -0500

    Added first person shooter demo

---
+++ 07a_and_07b_FirstPersonController/Assets/MouseLook2.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook2 : MonoBehaviour {
+    [SerializeField] private float mouseSensitivity = 0.002f;
+    [SerializeField] private Transform playerBody;
+
+    private float verticalRotation = 0.0f;
+
+    void Awake() {
+        Cursor.lockState = CursorLockMode.Locked;
+
+        verticalRotation = 0.0f;
+    }
+
+    void Update() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        verticalRotation += mouseY;
+
+        if (verticalRotation > 90.0f) {
+            // looking up more than 90 degrees
+            verticalRotation = 90.0f;
+            fixVerticalRotation(270.0f);
+        } else if (verticalRotation < -90.0f) {
+            verticalRotation = -90.0f;
+            fixVerticalRotation(90.0f);
+        } else {
+            transform.Rotate(Vector3.left * mouseY);
+        }
+
+        playerBody.transform.Rotate(Vector3.up * mouseX);
+    }
+
+    private void fixVerticalRotation(float verticalRotation) {
+        Vector3 angles = transform.eulerAngles;
+        angles.x = verticalRotation;
+        transform.eulerAngles = angles;
+    }
+}
---
+++ 10a_MeleeCombat_BaseProject/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private Transform target = null;
+
+    void Start() {
+        transform.position = target.position + offset;
+    }
+
+    void Update() {
+        transform.position = target.position + offset;
+    }
+}
