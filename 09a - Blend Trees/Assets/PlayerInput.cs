596f12ab1dfd06621f2f1cd4d55161488fc2b392
randyfortier <randy.fortier@gmail.com>
2019-03-11 16:47:34 -0400

    Added completed animation blend trees demo

---
+++ 09a - Blend Trees/Assets/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    private Animator animator;
+
+    [SerializeField] float runSpeed = 1.0f;
+    [SerializeField] float turnSpeed = 1.0f;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        var x = Input.GetAxis("Horizontal") * turnSpeed;
+        var y = Input.GetAxis("Vertical") * runSpeed;
+
+        animator.SetFloat("Forward", y);
+        animator.SetFloat("Turn", x);
+    }
+}
