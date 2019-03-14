ea2b176410b2bdc3ae6f0c3fbb11e96467b70870
randyfortier <randy.fortier@gmail.com>
2019-03-14 16:14:45 -0400

    Added NavMesh demo

---
+++ 09b_Navigation/Assets/EnemyMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] float closeEnoughDistance;
+    [SerializeField] bool loop;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    private int waypointIndex = 0;
+    private bool patrolling = true;
+
+    private void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        if (agent != null) {
+            agent.SetDestination(waypoints[waypointIndex].position);
+        }
+    }
+
+    private void Update() {
+        if (!patrolling) {
+            return;
+        }
+
+        float distanceToWaypoint = Vector3.Distance(agent.transform.position, 
+                                                    waypoints[waypointIndex].position);
+
+        if (distanceToWaypoint < closeEnoughDistance) {
+            // we've arrived at the waypoint
+
+            // go to the next waypoint
+            waypointIndex++;
+
+            // loop, if desired
+            if (waypointIndex >= waypoints.Length) {
+                if (loop) {
+                    waypointIndex = 0;
+                } else {
+                    patrolling = false;
+                    return;
+                }
+            }
+
+            if (agent != null) {
+                agent.SetDestination(waypoints[waypointIndex].position);
+            }
+        }
+        animator.SetBool("Crouch", false);
+        animator.SetFloat("Forward", agent.velocity.magnitude);
+        //animator.SetFloat("Turn", agent.angularSpeed);
+    }
+}
---
+++ 09b_Navigation/Assets/PlayerInput2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput2 : MonoBehaviour {
+    [SerializeField] private float movementSpeed = 5.0f;
+    [SerializeField] private float jumpFactor = 5.0f;
+    [SerializeField] private AnimationCurve jumpCurve;
+
+    private CharacterController controller;
+
+    private bool isJumping = false;
+
+    void Awake() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update() {
+        // movement and strafing
+        float horizontal = Input.GetAxis("Horizontal") * movementSpeed;
+        float vertical = Input.GetAxis("Vertical") * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertical;
+        Vector3 rightMovement = transform.right * horizontal;
+
+        controller.SimpleMove(forwardMovement + rightMovement);
+
+        // jumping
+        if (Input.GetButtonDown("Jump") && !isJumping) {
+            isJumping = true;
+            StartCoroutine(Jump());
+        }
+
+    }
+
+    // co-routine
+    private IEnumerator Jump() {
+        float timeInAir = 0.0f;
+
+        do {
+            // update the position
+            float jumpAmount = jumpCurve.Evaluate(timeInAir) * jumpFactor * Time.deltaTime;
+            controller.Move(Vector3.up * jumpAmount);
+            timeInAir += Time.deltaTime;
+
+            yield return null;
+        } while (!controller.isGrounded && controller.collisionFlags != CollisionFlags.Above);
+
+        isJumping = false;
+    }
+}
