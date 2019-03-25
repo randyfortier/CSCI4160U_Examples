7a26a9bd22aca7dfb586c21b2f831388cce6b2fd
randyfortier <randy.fortier@gmail.com>
2019-03-25 11:30:08 -0400

    Added base project for shaders demo

---
+++ 12a_Shaders/Assets/Scripts/AutoRotator.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotator : MonoBehaviour {
+    private float rotation = 0.1f;
+
+    void Update() {
+        transform.Rotate(new Vector3(0.0f, rotation, 0.0f));
+    }
+}
