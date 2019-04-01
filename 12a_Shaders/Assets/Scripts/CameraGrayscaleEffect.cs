01f3e524a552c38100258fea42811750bcec6f39
randyfortier <randy.fortier@gmail.com>
2019-04-01 15:43:02 -0400

    Added shaders demo

---
+++ 12a_Shaders/Assets/Scripts/CameraGrayscaleEffect.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraGrayscaleEffect : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0, 1)]
+    private float effectAmount = 0.0f;
+
+    [SerializeField]
+    private bool autoFade = true;
+
+    [SerializeField]
+    private float fadeSpeed = 0.05f;
+
+    [SerializeField]
+    private Shader shader = null;
+
+    private Material material = null;
+
+    void Update()
+    {
+        if (autoFade) {
+            effectAmount -= fadeSpeed * Time.deltaTime;
+            effectAmount = Mathf.Max(effectAmount, 0.0f);
+        }
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (shader == null)
+            return;
+
+        if (material == null) {
+            material = new Material(shader);
+        }
+
+        // send data into shader
+        material.SetFloat("_EffectAmount", effectAmount);
+
+        // copy the pixels onto the destination image/texture
+        Graphics.Blit(source, destination, material);
+    }
+}
---
+++ 12a_Shaders/Assets/Shaders/FlatShader.shader
@@ -0,0 +1,48 @@
+﻿Shader "CSCI4160U/FlatShader"
+{
+    Properties
+    {
+        _Colour ("Colour", Color) = (1,1,1,1)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma multi_compile_fog
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 pos : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+            };
+
+            half4 _Colour;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.pos);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                return _Colour;
+            }
+            ENDCG
+        }
+    }
+}
