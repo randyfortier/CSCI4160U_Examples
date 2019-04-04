05d484e1873d202024c2cbe2986efd333d58ab8e
randyfortier <randy.fortier@gmail.com>
2019-04-04 14:03:46 -0400

    Added base project for VR demo

---
+++ 12b_VR/Assets/Target.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+    private void OnTriggerEnter(Collider other) {
+        Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
+}
