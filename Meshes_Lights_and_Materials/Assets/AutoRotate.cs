a154dbdae3c7623a0349b8a9802092218aa79643
randyfortier <randy.fortier@gmail.com>
2020-02-05 15:33:37 -0500

    Added lights, meshes, and materials demo

---
+++ Meshes_Lights_and_Materials/Assets/AutoRotate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour {
+    [SerializeField] float rotationSpeed = 0.0f;
+
+    private float rotationAngle = 0.0f;
+
+    void Update() {
+        rotationAngle += rotationSpeed * Time.deltaTime;
+
+        transform.rotation = Quaternion.Euler(0.0f, rotationAngle, 0.0f);
+    }
+}
